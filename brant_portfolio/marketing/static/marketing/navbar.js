687322d0208f42e55cf246e0bf4a7b5a50bc4e4c
Brant Morton <brantmort@gmail.com>
2019-09-19 16:56:41 +0000

    fixed navbar bug

--- brant_portfolio/marketing/static/marketing/navbar.js
+++ brant_portfolio/marketing/static/marketing/navbar.js
@@ -1,22 +1,25 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const mq = window.matchMedia( "(min-width: 800px)" );
+if (mq.matches) {
+    // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementById("navbar");
+  // Get the header
+  var header = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
 }
+  }
